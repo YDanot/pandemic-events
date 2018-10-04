6a45ad8abe70789f8e8d1af146195dcdeffc009a
YDanot <y.danot@gmail.com>
2018-10-04 15:18:43 +0200

    Refactoring: Epidemic algorithm

--- src/main/java/domain/epidemic/Epidemic.java
+++ src/main/java/domain/epidemic/Epidemic.java
@@ -14,24 +14,42 @@ public class Epidemic implements EpidemicListener {
 
     @Override
     public void onEpidemic() {
+        increaseInfectionRate();
+        infectLastDrawCardThreeTimes();
+        intensifyInfections();
+    }
+
+    private void increaseInfectionRate() {
         World.game.infectionRateTrack.moveUp();
+    }
+
+    private void infectLastDrawCardThreeTimes() {
         InfectionCard infectionCard = World.game.infectionCardsPiles.drawBottom();
         CityName cityName = CityName.valueOf(infectionCard.name());
         TurnId turnId = new TurnId();
         infect(infectionCard, cityName, turnId);
         infect(infectionCard, cityName, turnId);
         infect(infectionCard, cityName, turnId);
-
-        List<InfectionCard> discardPile = World.game.infectionCardsPiles.discardPile();
-        Stack<InfectionCard> drawPile = World.game.infectionCardsPiles.drawPile();
-        Collections.shuffle(discardPile);
-        discardPile.forEach(drawPile::push);
-        discardPile.clear();
-
     }
 
     private void infect(InfectionCard infectionCard, CityName cityName, TurnId turnId) {
         World.eventBus.publish(new InfectionEvent(infectionCard.disease(), cityName, turnId,
                 World.game.network.get(cityName).infectionLevelFor(infectionCard.disease())));
+    }
+
+    private void intensifyInfections() {
+        placeOnTopOfDrawPile(shuffleDiscardPile());
+    }
+
+    private void placeOnTopOfDrawPile(List<InfectionCard> discardPile) {
+        Stack<InfectionCard> drawPile = World.game.infectionCardsPiles.drawPile();
+        discardPile.forEach(drawPile::push);
+        discardPile.clear();
+    }
+
+    private List<InfectionCard> shuffleDiscardPile() {
+        List<InfectionCard> discardPile = World.game.infectionCardsPiles.discardPile();
+        Collections.shuffle(discardPile);
+        return discardPile;
     }
 }
